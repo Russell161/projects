9459d2fb4e73864a571901311d78e2522dd3dff1
Russell161 <rleggjunior@gmail.com>
2025-08-14 16:02:31 -0600

    aligned photo+content

---
+++ qr_code/qr-code-component-main/design/qrcode.js
@@ -0,0 +1,10 @@
+// import qrcode from 'node_modules/qrcode-generator/dist/qrcode.js';
+import { qrcode } from 'qrcode-generator';
+
+var typeNumber = 4;
+var errorCorrectionLevel = 'L';
+var qr = qrcode(typeNumber, errorCorrectionLevel);
+qr.addData('Hi!');
+qr.make();
+document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+
